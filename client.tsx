779f245c12e824d8828e81784d6f5c40cb573dc4
sm9171@nate.com <sm9171@nate.com>
2021-02-28 16:57:41 +0900

    first

---
+++ client.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from '@layouts/App';
+import { BrowserRouter } from 'react-router-dom';
+
+render(
+    <BrowserRouter><App /></BrowserRouter>,
+    document.querySelector('#app'),
+);
+
+// pages - 서비스 페이지
+// components - 짜잘 컴포넌트
+// layouts - 공통 레이아웃
